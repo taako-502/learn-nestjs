7fdb8367488a6e684b9cfbdb0beed9470a210619
taako-502 <i.am.armorknight@gmail.com>
2022-12-07 02:01:09 +0900

    chore: Sequelizeを導入

---
+++ migrations/20221206155944-create-user.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      birth: {
+        type: Sequelize.DATE
+      },
+      country_code: {
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ migrations/README.md
@@ -0,0 +1,18 @@
+# migration
+以下のコマンドでmigrationファイルを作成する。
+```bash
+node_modules/.bin/sequelize model:create --name user --underscored --attributes name:string,birth:date,country_code:integer
+```
+
+migrateする
+```bash
+$ node_modules/.bin/sequelize db:migrate
+
+# 環境を指定する場合
+$ node_modules/.bin/sequelize db:migrate --env development
+```
+
+ロールバックする
+```bash
+node_modules/.bin/sequelize db:migrate:undo --env development
+```
