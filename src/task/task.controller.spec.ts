f5da75452ef28683f395e71ae24986aa0b02512f
taako-502 <i.am.armorknight@gmail.com>
2022-12-12 01:12:14 +0900

    test: テストのエラーが解消

--- src/task/task.controller.spec.ts
+++ src/task/task.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskController } from './task.controller';
+import { TaskService } from './task.service';
+import { CreateTaskDTO } from './task.dto';
 
 describe('TaskController', () => {
   let controller: TaskController;
@@ -7,6 +9,36 @@ describe('TaskController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TaskController],
+      providers: [
+        {
+          provide: TaskService,
+          useValue: {
+            create: jest
+              .fn()
+              .mockImplementation((user: CreateTaskDTO) =>
+                Promise.resolve({ id: '1', ...user }),
+              ),
+            findAll: jest.fn().mockResolvedValue([
+              {
+                firstName: 'firstName #1',
+                lastName: 'lastName #1',
+              },
+              {
+                firstName: 'firstName #2',
+                lastName: 'lastName #2',
+              },
+            ]),
+            findOne: jest.fn().mockImplementation((id: string) =>
+              Promise.resolve({
+                firstName: 'firstName #1',
+                lastName: 'lastName #1',
+                id,
+              }),
+            ),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<TaskController>(TaskController);
