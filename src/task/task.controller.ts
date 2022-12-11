8f64b611258875f17a39c80097b8fa01187b3fc5
taako-502 <i.am.armorknight@gmail.com>
2022-12-12 01:31:54 +0900

    test: テストが動作するよう変更

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -8,18 +8,18 @@ import { InsertResult } from 'typeorm';
 export class TaskController {
     constructor(private readonly service: TaskService) {}
 
-    // @Get()
-    // async getTaskList(): Promise<Task[]> {
-    //     return await this.service.findAll();
-    // }
+    @Get()
+    async getTaskList(): Promise<Task[]> {
+        return await this.service.findAll();
+    }
 
-    // @Post()
-    // async addTask(@Body() task: CreateTaskDTO): Promise<InsertResult> {
-    //     return await this.service.create(task);
-    // }
+    @Post()
+    async addTask(@Body() task: CreateTaskDTO): Promise<InsertResult> {
+        return await this.service.create(task);
+    }
 
-    // @Get(':id')
-    // async getTask(@Param('id') id: string): Promise<Task> {
-    //     return await this.service.find(Number(id));
-    // }
+    @Get(':id')
+    async getTask(@Param('id') id: string): Promise<Task> {
+        return await this.service.find(Number(id));
+    }
 }
--- src/task/task.service.spec.ts
+++ src/task/task.service.spec.ts
@@ -1,12 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskService } from './task.service';
+import { CreateTaskDTO } from './task.dto';
+
 
 describe('TaskService', () => {
   let service: TaskService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TaskService],
+      providers: [
+        {
+          provide: TaskService,
+          useValue: {
+            create: jest
+              .fn()
+              .mockImplementation((user: CreateTaskDTO) =>
+                Promise.resolve({ id: '1', ...user }),
+              ),
+            findAll: jest.fn().mockResolvedValue([
+              {
+                firstName: 'firstName #1',
+                lastName: 'lastName #1',
+              },
+              {
+                firstName: 'firstName #2',
+                lastName: 'lastName #2',
+              },
+            ]),
+            findOne: jest.fn().mockImplementation((id: string) =>
+              Promise.resolve({
+                firstName: 'firstName #1',
+                lastName: 'lastName #1',
+                id,
+              }),
+            ),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<TaskService>(TaskService);
